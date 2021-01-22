61b8b736b4392ede61c62fbfd3792c7c2ca5f3c5
Hunter Laine <hunterglaine@gmail.com>
2021-01-22 13:02:12 -0800

    Create __init__ and __repr__ methods for Customer class

--- customers.py
+++ customers.py
@@ -4,4 +4,22 @@
 class Customer(object):
     """Ubermelon customer."""
 
+    def __init__(self, 
+                first_name, 
+                last_name, 
+                email, 
+                password):
+        """Creates a Customer object with intance attributes, first name, last 
+        name, email, and password"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+
+
+    def __repr__(self):
+        """Method to show customer info more clearly in console"""
+
+        return f"<Customer: {self.first_name} {self.last_name}, {self.email}>"
     # TODO: need to implement this
