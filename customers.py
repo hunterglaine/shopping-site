0669fb9d66e1852886c1994e2713675a00b6ef40
Hunter Laine <hunterglaine@gmail.com>
2021-01-22 15:26:07 -0800

    Create read_customers_from_file function to create customer dictionary

--- customers.py
+++ customers.py
@@ -23,3 +23,21 @@ class Customer(object):
 
         return f"<Customer: {self.first_name} {self.last_name}, {self.email}>"
     # TODO: need to implement this
+
+
+def read_customers_from_file(filepath):
+    """Read customer data and create a dictionary of customers"""
+
+    customers = {}
+
+    for line in open(filepath):
+        user_info = line.rstrip().split("|")
+
+        customers[user_info[2]] = Customer(user_info[0], 
+                                            user_info[1], 
+                                            user_info[2], 
+                                            user_info[3])
+
+    return customers
+
+
