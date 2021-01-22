9dbef579ceb4f973444bd4b93c922bc31f66af4f
Hunter Laine <hunterglaine@gmail.com>
2021-01-21 17:03:58 -0800

    Fix show_melon function for melon info page, import session

--- shoppingsite.py
+++ shoppingsite.py
@@ -6,12 +6,13 @@ put melons in a shopping cart.
 Authors: Joel Burton, Christian Fernandez, Meggie Mahnken, Katie Byers.
 """
 
-from flask import Flask, render_template, redirect, flash
+from flask import Flask, render_template, redirect, flash, session
 import jinja2
 
 import melons
 
 app = Flask(__name__)
+app.secret_key = "FG%64yuhfnG532NgfehjwHGKLP*897Rr"
 
 # A secret key is needed to use Flask sessioning features
 app.secret_key = 'this-should-be-something-unguessable'
@@ -50,7 +51,7 @@ def show_melon(melon_id):
     Show all info about a melon. Also, provide a button to buy that melon.
     """
 
-    melon = melons.get_by_id("meli")
+    melon = melons.get_by_id(melon_id)
     print(melon)
     return render_template("melon_details.html",
                            display_melon=melon)
